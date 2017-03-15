54914982caa17da456b0013bafeb86dcffa710ef
Ian Hill <ianhill@amazon.com>
2017-03-15 11:22:08 -0700

    Fixed issue with CentOS incorrect time in AWSLogs. AWSLogs now use the instance_id as the stream_name. Removed /var/log/messages from AWSLogs config

--- scripts/bastion_bootstrap.sh
+++ scripts/bastion_bootstrap.sh
@@ -144,15 +144,14 @@ cat <<'EOF' >> ~/cloudwatchlog.conf
 [/var/log/bastion]
 datetime_format = %b %d %H:%M:%S
 buffer_duration = 5000
-log_stream_name = bastion
+log_stream_name = {instance_id}
 initial_position = start_of_file
 EOF
 
-    export STREAM_NAME=`cat /etc/awslogs/awslogs.conf | grep ^log_stream_name | head -1`
-    export TMPGROUP=`cat /etc/awslogs/awslogs.conf | grep ^log_group_name`
-    export TMPGROUP=`echo $TMPGROUP | sed 's/\//\\\\\//g'`
-    sed -i.back "s/$STREAM_NAME/log_stream_name = messages/g" /etc/awslogs/awslogs.conf
-    sed -i.back "s/$TMPGROUP/log_group_name = $CWG/g" /etc/awslogs/awslogs.conf
+    LINE=$(cat -n /etc/awslogs/awslogs.conf | grep '\[\/var\/log\/messages\]' | awk {'print $1'})
+    END_LINE=$(echo $(($LINE-1)))
+    head -$END_LINE /etc/awslogs/awslogs.conf > /tmp/awslogs.conf
+    cat /tmp/awslogs.conf > /etc/awslogs/awslogs.conf
     cat ~/cloudwatchlog.conf >> /etc/awslogs/awslogs.conf
     cat /tmp/groupname.txt >> /etc/awslogs/awslogs.conf
     export TMPREGION=`cat /etc/awslogs/awscli.conf | grep region`
@@ -207,7 +206,7 @@ cat <<'EOF' >> ~/cloudwatchlog.conf
 state_file = /var/awslogs/state/agent-state
 
 [/var/log/bastion]
-log_stream_name = bastion
+log_stream_name = {instance_id}
 datetime_format = %b %d %H:%M:%S
 EOF
     export Region=`curl http://169.254.169.254/latest/meta-data/placement/availability-zone | rev | cut -c 2- | rev`
@@ -272,7 +271,7 @@ EOF
     echo "declare -rx BASTION_LOG=${BASTION_MNT}/${BASTION_LOG}" >> /etc/bashrc
 
 cat <<'EOF' >> /etc/bashrc
-declare -rx PROMPT_COMMAND='history -a >(logger -t "ON: ${TIME}   [FROM]:${IP}   [USER]:${USER}   [PWD]:${PWD}" -s 2>>${BASTION_LOG})'
+declare -rx PROMPT_COMMAND='history -a >(logger -t "ON: $(date)   [FROM]:${IP}   [USER]:${USER}   [PWD]:${PWD}" -s 2>>${BASTION_LOG})'
 EOF
     chown root:centos ${BASTION_MNT}
     chown root:centos /usr/bin/script
@@ -301,7 +300,7 @@ loggin_config_file = /var/awslogs/etc/awslogs.conf
 datetime_format = %Y-%m-%d %H:%M:%S
 file = /var/log/messages
 buffer_duration = 5000
-log_stream_name = bastion
+log_stream_name = {instance_id}
 initial_position = start_of_file
 EOF
         export Region=`curl http://169.254.169.254/latest/meta-data/placement/availability-zone | rev | cut -c 2- | rev`
@@ -350,7 +349,7 @@ cat <<'EOF' >> ~/cloudwatchlog.conf
 [/var/log/bastion]
 datetime_format = %b %d %H:%M:%S
 buffer_duration = 5000
-log_stream_name = bastion
+log_stream_name = {instance_id}
 initial_position = start_of_file
 EOF
         export TMPGROUP=`cat /etc/awslogs/awslogs.conf | grep ^log_group_name`
