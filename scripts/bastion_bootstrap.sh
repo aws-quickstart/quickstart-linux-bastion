86f93ecba45eed31837fabe5b286df0ff1572d99
Santiago Cardenas <sancard@amazon.com>
2017-03-17 19:14:34 -0700

    Reordered EIP logic to reduce duplication. Added extra log messages.

--- scripts/bastion_bootstrap.sh
+++ scripts/bastion_bootstrap.sh
@@ -52,6 +52,7 @@ function osrelease () {
     else
         echo "Operating System Not Found"
     fi
+    echo "${FUNCNAME[0]} Ended" >> /var/log/cfn-init.log
 }
 
 function harden_ssh_security () {
@@ -388,68 +389,67 @@ function request_eip() {
     ASSIGNED=$(aws ec2 describe-addresses --region $Region --output text | grep $INSTANCE_IP | wc -l)
     if [ "$ASSIGNED" -gt "$ZERO" ]; then
         echo "Already assigned an EIP."
-        exit 0;
-    fi
-
-    aws ec2 describe-addresses --region $Region --output text > /query.txt
-    #Ensure we are only using EIPs from our Stack
-    line=`curl http://169.254.169.254/latest/user-data/ | grep EIP_LIST`
-    IFS=$':' DIRS=(${line//$','/:})       # Replace tabs with colons.
-
-    for (( i=0 ; i<${#DIRS[@]} ; i++ )); do
-        EIP=`echo ${DIRS[i]} | sed 's/\"//g' | sed 's/EIP_LIST=//g'`
-        if [ $EIP != "Null" ]; then
-            #echo "$i: $EIP"
-            grep "$EIP" /query.txt >> /query2.txt;
-        fi
-    done
-    mv /query2.txt /query.txt
-
-
-    AVAILABLE_EIPs=`cat /query.txt | wc -l`
-
-    if [ "$AVAILABLE_EIPs" -gt "$ZERO" ]; then
-        FIELD_COUNT="5"
-        INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
-        echo "Running associate_eip_now"
-        while read name;
-        do
-            #EIP_ENTRY=$(echo $name | grep eip | wc -l)
-            EIP_ENTRY=$(echo $name | grep eni | wc -l)
-            echo "EIP: $EIP_ENTRY"
-            if [ "$EIP_ENTRY" -eq 1 ]; then
-                echo "Already associated with an instance"
-                echo ""
-            else
-                export EIP=`echo "$name" | sed 's/[\s]+/,/g' | awk {'print $4'}`
-                EIPALLOC=`echo $name | awk {'print $2'}`
-                echo "NAME: $name"
-                echo "EIP: $EIP"
-                echo "EIPALLOC: $EIPALLOC"
-                aws ec2 associate-address --instance-id $INSTANCE_ID --allocation-id $EIPALLOC --region $Region
-            fi
-        done < /query.txt
     else
-        echo "None available in this Region"
-        exit 1
-    fi
+        aws ec2 describe-addresses --region $Region --output text > /query.txt
+        #Ensure we are only using EIPs from our Stack
+        line=`curl http://169.254.169.254/latest/user-data/ | grep EIP_LIST`
+        IFS=$':' DIRS=(${line//$','/:})       # Replace tabs with colons.
 
-    #Retry
-    INSTANCE_IP=`ifconfig -a | grep inet | awk {'print $2'} | sed 's/addr://g' | head -1`
-    ASSIGNED=$(aws ec2 describe-addresses --region $Region --output text | grep $INSTANCE_IP | wc -l)
-    if [ "$ASSIGNED" -eq 1 ]; then
-        echo "Already assigned an EIP"
-        exit 0;
-    fi
-    while [ "$ASSIGNED" -eq "$ZERO" ]
-    do
-        sleep 3
-        request_eip
+        for (( i=0 ; i<${#DIRS[@]} ; i++ )); do
+            EIP=`echo ${DIRS[i]} | sed 's/\"//g' | sed 's/EIP_LIST=//g'`
+            if [ $EIP != "Null" ]; then
+                #echo "$i: $EIP"
+                grep "$EIP" /query.txt >> /query2.txt;
+            fi
+        done
+        mv /query2.txt /query.txt
+
+
+        AVAILABLE_EIPs=`cat /query.txt | wc -l`
+
+        if [ "$AVAILABLE_EIPs" -gt "$ZERO" ]; then
+            FIELD_COUNT="5"
+            INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+            echo "Running associate_eip_now"
+            while read name;
+            do
+                #EIP_ENTRY=$(echo $name | grep eip | wc -l)
+                EIP_ENTRY=$(echo $name | grep eni | wc -l)
+                echo "EIP: $EIP_ENTRY"
+                if [ "$EIP_ENTRY" -eq 1 ]; then
+                    echo "Already associated with an instance"
+                    echo ""
+                else
+                    export EIP=`echo "$name" | sed 's/[\s]+/,/g' | awk {'print $4'}`
+                    EIPALLOC=`echo $name | awk {'print $2'}`
+                    echo "NAME: $name"
+                    echo "EIP: $EIP"
+                    echo "EIPALLOC: $EIPALLOC"
+                    aws ec2 associate-address --instance-id $INSTANCE_ID --allocation-id $EIPALLOC --region $Region
+                fi
+            done < /query.txt
+        else
+            echo "[ERROR] Elastic IPs available in this region"
+            exit 1
+        fi
+
         INSTANCE_IP=`ifconfig -a | grep inet | awk {'print $2'} | sed 's/addr://g' | head -1`
         ASSIGNED=$(aws ec2 describe-addresses --region $Region --output text | grep $INSTANCE_IP | wc -l)
-    done
+        if [ "$ASSIGNED" -eq 1 ]; then
+            echo "EIP successfully assigned."
+        else
+            #Retry
+            while [ "$ASSIGNED" -eq "$ZERO" ]
+            do
+                sleep 3
+                request_eip
+                INSTANCE_IP=`ifconfig -a | grep inet | awk {'print $2'} | sed 's/addr://g' | head -1`
+                ASSIGNED=$(aws ec2 describe-addresses --region $Region --output text | grep $INSTANCE_IP | wc -l)
+            done
+        fi
+    fi
+
     echo "${FUNCNAME[0]} Ended"
-    exit 0
 }
 
 function call_request_eip() {
@@ -459,16 +459,13 @@ function call_request_eip() {
     ASSIGNED=$(aws ec2 describe-addresses --region $Region --output text | grep $INSTANCE_IP | wc -l)
     if [ "$ASSIGNED" -gt "$ZERO" ]; then
         echo "Already assigned an EIP."
-        exit 0
+    else
+        WAIT=$(shuf -i 1-30 -n 1)
+        sleep "$WAIT"
+        request_eip
     fi
-    WAIT=$(shuf -i 1-30 -n 1)
-    sleep "$WAIT"
-    request_eip
     echo "${FUNCNAME[0]} Ended"
-    exit 0
 }
-
-
 
 ##################################### End Function Definitions
 
@@ -590,3 +587,5 @@ else
 fi
 
 call_request_eip
+
+echo "Bootstrap complete."
