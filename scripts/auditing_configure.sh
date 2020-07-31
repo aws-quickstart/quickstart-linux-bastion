aaa9625d60efeb825271b31ddc6acb8084e89185
Andrew Glenn <29951057+andrew-glenn@users.noreply.github.com>
2020-07-30 20:18:34 -0500

    Adding auditing config outside of bootstrap

---
+++ scripts/auditing_configure.sh
@@ -0,0 +1,21 @@
+function install_stuff_ubuntu(){
+  apt-get -y install auditd
+}
+
+function add_the_rules(){
+  cat /tmp/auditd.rules >> /etc/audit/rules.d/audit.rules
+  rm /tmp/auditd.rules
+}
+
+function restart_services(){
+  service auditd restart
+}
+
+case "${BASTION_OS}" in 
+    "Ubuntu")
+      install_stuff_ubuntu
+      ;;
+esac
+
+add_the_rules
+restart_services
